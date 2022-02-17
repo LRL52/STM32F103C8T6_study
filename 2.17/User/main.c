207645f4c8e93d8997bf4762c0b06dba9c79fc37
LRL52 <525687841@qq.com>
2022-02-17 16:59:48 +0800

    发送和接收字符串(接收会莫名丢包)

--- 2.17/User/main.c
+++ 2.17/User/main.c
@@ -7,8 +7,14 @@ char text[] = "Hello world!\r\n  ¡ª¡ªsent by LRL52 on Feb 17th, 2022";
 
 int main(void){
 	LED_GPIO_Config();
+	//delay_init();
 	LED_ON();
 	My_USART1();
 	USART1_SendString(text);
-	while(1);
+	static char s[2] = {0};
+	while(1){
+		while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
+		s[0] = USART_ReceiveData(USART1);
+		USART1_SendString(s);
+	}
 }
